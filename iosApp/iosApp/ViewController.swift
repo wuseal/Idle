3c089c76df739008bb90e41da8b838fe7fb7f8ed
Seal <wusealking@gmail.com>
2019-07-28 18:54:48 +0800

    Initial commit

---
+++ iosApp/iosApp/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+import app
+import AFNetworking
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = Proxy().proxyHello()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBOutlet weak var label: UILabel!
+}
