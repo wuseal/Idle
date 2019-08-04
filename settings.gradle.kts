a56b37ac1837a49ba259e744f34bb760df7a595e
Seal <wusealking@gmail.com>
2019-08-04 23:44:54 +0800

    migrate androidapp project build.gradle to build.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        gradlePluginPortal()
     }
 
     resolutionStrategy {
