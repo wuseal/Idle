7a10b602d6941255f1641d15f98e730fefd60d35
Seal <wusealking@gmail.com>
2019-09-07 19:41:13 +0800

    convert java test to kotlin file

--- androidapp/src/androidTest/java/wu/seal/mpp/idle/ExampleInstrumentedTest.kt
+++ androidapp/src/androidTest/java/wu/seal/mpp/idle/ExampleInstrumentedTest.kt
@@ -1,26 +1,26 @@
-package wu.seal.mpp.idle;
+package wu.seal.mpp.idle
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import static org.junit.Assert.*;
+import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        val appContext = InstrumentationRegistry.getTargetContext()
 
-        assertEquals("wu.seal.mpp.idle.test", appContext.getPackageName());
+        assertEquals("wu.seal.mpp.idle.test", appContext.packageName)
     }
 }
